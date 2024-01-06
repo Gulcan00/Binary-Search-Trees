5ca48a3fe34bdd48d4f7380004136c76eebf6b9c
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2024-01-06 17:53:03 +0200

    Complete tree functions

--- Tree.js
+++ Tree.js
@@ -1,8 +1,8 @@
 const createNode = require("./Node");
 
-function createTree(arr) {
+export function createTree(arr) {
   function sortAndRemoveDuplicates(arr) {
-    arr.sort();
+    arr.sort((a, b) => a - b);
     const duplicatesRemoved = [...new Set(arr)];
     return duplicatesRemoved;
   }
@@ -39,11 +39,214 @@ function createTree(arr) {
     console.log(prettyPrint(root));
   }
 
+  function insert(value, node = root) {
+    if (!node) {
+      return createNode(value);
+    }
+    if (value < node.data) {
+      node.left = insert(value, node.left);
+    } else {
+      node.right = insert(value, node.right);
+    }
+
+    return node;
+  }
+
+  function remove(value, node = root) {
+    if (!node) {
+      return null;
+    }
+    if (value < node.data) {
+      node.left = remove(value, node.left);
+    } else if (value > node.data) {
+      node.right = remove(value, node.right);
+    } else {
+      if (!node.left && !node.right) {
+        return null;
+      } else if (node.left && node.right) {
+        let inOrderSuccessor = node.right;
+        while (inOrderSuccessor.left) {
+          inOrderSuccessor = inOrderSuccessor.left;
+        }
+        node.data = inOrderSuccessor.data;
+        node.right = remove(inOrderSuccessor.data, node.right);
+      } else {
+        return node.left || node.right;
+      }
+    }
+
+    return node;
+  }
+
+  function find(value, node = root) {
+    if (!node) {
+      return null;
+    }
+    if (value === node.data) {
+      return node;
+    }
+    if (value < node.data) {
+      return find(value, node.left);
+    } else {
+      return find(value, node.right);
+    }
+  }
+
+  function levelOrderIt(cb) {
+    const queue = [root];
+    const values = [];
+    while (queue.length > 0) {
+      const node = queue.shift();
+      if (cb) {
+        cb(node);
+      } else {
+        values.push(node.data);
+      }
+
+      queue.push(node.left);
+      queue.push(node.right);
+    }
+
+    if (!cb) {
+      return values;
+    }
+  }
+
+  function levelOrderRec(cb, queue = [root], values = []) {
+    if (queue.length > 0) {
+      const node = queue.shift();
+      if (cb) {
+        cb(node);
+      } else {
+        values.push(node.data);
+      }
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    if (queue.length > 0) {
+      levelOrderRec(cb, queue);
+    }
+
+    if (!cb) {
+      return values;
+    }
+  }
+
+  function inOrder(cb, node = root, values = []) {
+    if (node) {
+      inOrder(cb, node.left, values);
+      if (cb) {
+        cb(node.data);
+      } else {
+        values.push(node.data);
+      }
+      inOrder(cb, node.right, values);
+    }
+
+    if (!cb) {
+      return values;
+    }
+  }
+
+  function preOrder(cb, node = root, values = []) {
+    if (node) {
+      if (cb) {
+        cb(node.data);
+      } else {
+        values.push(node.data);
+      }
+      inOrder(cb, node.left, values);
+      inOrder(cb, node.right, values);
+    }
+
+    if (!cb) {
+      return values;
+    }
+  }
+
+  function postOrder(cb, node = root, values = []) {
+    if (node) {
+      inOrder(cb, node.left, values);
+      inOrder(cb, node.right, values);
+      if (cb) {
+        cb(node.data);
+      } else {
+        values.push(node.data);
+      }
+    }
+
+    if (!cb) {
+      return values;
+    }
+  }
+
+  function height(node) {
+    if (!node) {
+      return 0;
+    }
+
+    const leftHeight = height(node.left);
+    const rightHeight = height(node.right);
+
+    return 1 + Math.max(leftHeight, rightHeight);
+  }
+
+  function depth(node, root = this.root) {
+    if (!root) {
+      return -1;
+    }
+
+    if (root.data === node.data) {
+      return 0;
+    }
+
+    const left = depth(node, root.left);
+    const right = depth(node, node.right);
+
+    if (left === -1 && right === -1) {
+      return -1;
+    }
+    return 1 + Math.max(left, right);
+  }
+
+  function isBalanced(node = root) {
+    if (!node) {
+      return true;
+    }
+    const leftHeight = height(node.left);
+    const rightHeight = height(node.right);
+
+    if (Math.abs(leftHeight - rightHeight) > 1) {
+      return false;
+    }
+
+    return isBalanced(node.left) && isBalanced(node.right);
+  }
+
+  function reBalance() {
+    const newArr = [];
+    inOrder(newArr.push);
+    root = buildTree(newArr);
+  }
+
   return {
     print,
+    insert,
+    remove,
+    find,
+    levelOrderIt,
+    levelOrderRec,
+    inOrder,
+    preOrder,
+    postOrder,
+    height,
+    depth,
+    isBalanced,
+    reBalance,
   };
 }
-
-const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const tree = createTree(arr);
-tree.print();
